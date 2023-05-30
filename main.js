155e0839e75c0360f47f7a5e31246f07c60e1ca3
thiago alves <thiago87gsa@gmail.com>
2023-05-30 18:56:42 -0300

    add funçoes aos botoes gramas e mililitros

---
+++ main.js
@@ -0,0 +1,13 @@
+let gramaMililitro = document.querySelector('.menu')
+let peso = document.querySelectorAll('.peso')
+let preco = document.querySelectorAll('.form-preco input')
+preco[0].focus()
+
+gramaMililitro.addEventListener('click',(e)=>{
+    let conteudoLi = e.target.textContent;
+    peso.forEach(i =>{
+        i.textContent = conteudoLi
+    })
+
+    preco[0].focus()
+})
