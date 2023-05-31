8b3bac605684eb7af5d8c9bad303f3b364b2b5d2
thiago alves <thiago87gsa@gmail.com>
2023-05-30 21:48:24 -0300

    colocando todas as funçoes no js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let gramaMililitro = document.querySelector('.menu')
 let peso = document.querySelectorAll('.peso')
-let preco = document.querySelectorAll('.form-peso input')
-preco[0].focus()
+let inputPreco = document.querySelectorAll('.form-peso input')
+let calcular = document.querySelector('#btn-calcular')
+let resultado = document.querySelector('.resultado')
+let frente = document.querySelector('.frente')
+let resFinal = document.querySelector('.resultado-final')
+let maisB = document.querySelector('.mais-barato')
+let campoResultado = document.querySelectorAll('.campo-resultado')
+let btnNovoCacl = document.querySelector('.btn-novo-calculo')
+
+inputPreco[0].focus()
 
 gramaMililitro.addEventListener('click',(e)=>{
     let conteudoLi = e.target.textContent;
@@ -9,5 +17,78 @@ gramaMililitro.addEventListener('click',(e)=>{
         i.textContent = conteudoLi
     })
 
-    preco[0].focus()
+    inputPreco[0].focus()
 })
+
+calcular.addEventListener('click', calcularPreco)
+btnNovoCacl.addEventListener('click', novoCalculo)
+
+function novoCalculo(){
+    resultado.style.display = 'none'
+    frente.style.display = 'flex'
+
+    document.querySelector('#peso1').value = ''
+    document.querySelector('#peso2').value = ''
+    document.querySelector('#preco1').value = ''
+    document.querySelector('#preco2').value = ''
+
+    peso1.textContent = ' '
+}
+
+function calcularPreco(){
+    let peso1 = Number(document.querySelector('#peso1').value)
+    let peso2 = Number(document.querySelector('#peso2').value)
+    let preco1 = Number(document.querySelector('#preco1').value)
+    let preco2 = Number(document.querySelector('#preco2').value)
+
+    if(peso1 === 0 || peso2 === 0 || preco1 === 0 || preco2 === 0) {
+        alert('Nenhum campo pode ficar vazio')
+    }else{
+        peso.forEach(i =>{
+            if(i.textContent === 'Gramas (g)'){
+                let precoPorPesoProduto1 = preco1 / peso1
+                let precoPorPesoProduto2 = preco2 / peso2
+           
+                let pesoBarato
+                if(precoPorPesoProduto1 < precoPorPesoProduto2){
+                    pesoBarato = `${peso1}(g)`
+                    maisB.innerHTML = `Produto de ${pesoBarato} é o mais barato`
+                }else if (precoPorPesoProduto1 > precoPorPesoProduto2){
+                    pesoBarato = `${peso2}(g)`
+                    maisB.innerHTML = `Produto de ${pesoBarato} é o mais barato`
+                }else{
+                    maisB.innerHTML = `Os dois prudutos estão com o mesmo preço`
+                }
+                
+                campoResultado[0].innerHTML = `O preço por grama do primeiro produto (com ${peso1} gramas) é de ${precoPorPesoProduto1.toFixed(2).padEnd(4, '0')} centavos`
+
+                campoResultado[1].innerHTML = `O preço por grama do segundo produto (com ${peso2} gramas) é de ${precoPorPesoProduto2.toFixed(2).padEnd(4, '0')} centavos`
+    
+                resultado.style.display = 'flex'
+                frente.style.display = 'none'
+            }else{
+                let precoPorPesoProduto1 = preco1 / peso1
+                let precoPorPesoProduto2 = preco2 / peso2
+           
+                let pesoBarato
+                if(precoPorPesoProduto1 < precoPorPesoProduto2){
+                    pesoBarato = `${peso1}(ml)`
+                    maisB.innerHTML = `Produto de ${pesoBarato} é o mais barato`
+                }else if (precoPorPesoProduto1 > precoPorPesoProduto2){
+                    pesoBarato = `${peso2}(ml)`
+                    maisB.innerHTML = `Produto de ${pesoBarato} é o mais barato`
+                }else{
+                    maisB.innerHTML = `Os dois prudutos estão com o mesmo preço`
+                }
+                
+                campoResultado[0].innerHTML = `O preço por mililitro do primeiro produto (com ${peso1} mililitros) é de ${precoPorPesoProduto1.toFixed(2).padEnd(4, '0')} centavos`
+
+                campoResultado[1].innerHTML = `O preço por mililitro do segundo produto (com ${peso2} mililitros) é de ${precoPorPesoProduto2.toFixed(2).padEnd(4, '0')} centavos`
+    
+                resultado.style.display = 'flex'
+                frente.style.display = 'none'
+            }
+        })
+          
+    }   
+}
